918322250ce00db98084839c047285543daa84d1
HIM <josekamaemaina2005@gmail.com>
2026-07-14 15:34:04 +0300

    fix: add requireRole import and delete route to organisations

--- src/routes/organisationRoutes.js
+++ src/routes/organisationRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require('bcrypt')
 const pool = require("../db")
-const {protect , requireAdmin} = require('../middleware/auth')
+const {protect , requireRole} = require('../middleware/auth')
 const {sendSuccess , sendError} = require('../utils/response')
 
 
